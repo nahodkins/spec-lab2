8c03bf0d81b758ed946f6ae2ace53f9b2fe281d1
nahodkins <tsuber2001@gmail.com>
2021-10-10 20:10:20 +0300

    Lab2 - Added fifth task implementation

---
+++ src/main/java/TrafficLightsColor.java
@@ -0,0 +1,35 @@
+import java.util.Arrays;
+
+public enum TrafficLightsColor {
+
+    RED("red"),
+    YELLOW("yellow"),
+    GREEN("green");
+
+    private static final String WRONG_COLOR_NUM_MSG_TEMPLATE = "There is no color with color number %d";
+    private static final String TO_STRING_MSG_TEMPLATE = "TrafficLightsColor:{colorName='%s'}";
+
+    private final String colorName;
+
+    TrafficLightsColor(String colorName) {
+        this.colorName = colorName;
+    }
+
+    public static TrafficLightsColor getColorByColorNum(int colorNum) {
+        switch (colorNum) {
+            case 1: return RED;
+            case 2: return YELLOW;
+            case 3: return GREEN;
+            default: throw new IllegalArgumentException(String.format(WRONG_COLOR_NUM_MSG_TEMPLATE, colorNum));
+        }
+    }
+
+    public String getColorName() {
+        return colorName;
+    }
+
+    @Override
+    public String toString() {
+        return String.format(TO_STRING_MSG_TEMPLATE, colorName);
+    }
+}

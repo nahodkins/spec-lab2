ab1db308aaa25bbdead2aa9cf3744c975e1f87f4
nahodkins <tsuber2001@gmail.com>
2021-10-10 19:49:12 +0300

    Lab2 - Added fourth task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,39 @@
+import java.util.Arrays;
+import java.util.stream.Collectors;
+
 public class Main {
 
+    private static final String ARRAY_TO_STRING_DELIMITER = ", ";
+
+    public static int[] getIntegerArray() {
+        return new int[] {1, 2, 3, 4, 5, 6};
+    }
+
+    public static String[] getStringArray() {
+        return new String[]{"str1", "str2", "str3", "str4"};
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int[] integerArray = getIntegerArray();
+        String[] stringArray = getStringArray();
+
+        System.out.println(Arrays.toString(integerArray));
+        System.out.println(Arrays.toString(stringArray));
+        System.out.println();
+
+        String integerToString = Arrays.stream(integerArray)
+                .boxed()
+                .map(String::valueOf)
+                .collect(Collectors.joining(ARRAY_TO_STRING_DELIMITER));
+        String stringArrayToString = String.join(ARRAY_TO_STRING_DELIMITER, stringArray);
+        System.out.println(integerToString);
+        System.out.println(stringArrayToString);
+        System.out.println();
+
+        Arrays.stream(integerArray)
+                .forEach(System.out::println);
+        System.out.println();
+        Arrays.stream(stringArray)
+                .forEach(System.out::println);
     }
 }

1183bd7bd4647162064f49604a1509a16a802d5c
nahodkins <tsuber2001@gmail.com>
2021-11-02 22:58:48 +0200

    Lab2 - Added 9th task implementation

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,6 +1,22 @@
+import java.util.ArrayList;
+import java.util.List;
+import java.util.stream.IntStream;
+
 public class Main {
 
+    private static int getListElementsMultiply(List<Integer> array) {
+        return array.stream()
+                .reduce((a, b) -> a * b)
+                .orElse(0);
+    }
+
     public static void main(String[] args) {
-        System.out.println("Hello World!");
+        int multiply = 0;
+        List<Integer> integers = new ArrayList<>();
+        while (multiply <= 10000) {
+            integers.add(integers.size() + 1);
+            multiply = getListElementsMultiply(integers);
+        }
+        System.out.println(integers);
     }
 }
